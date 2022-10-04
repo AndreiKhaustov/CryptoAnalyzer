6f3d75dc34cba2d9aa3b720ab51e915079469439
AndreiKhaustov <andrei.khaustov.nextlevel@gmail.com>
2022-10-04 15:11:59 +0300

    Add Decode algorithm

--- src/main/java/org/example/Decode.java
+++ src/main/java/org/example/Decode.java
@@ -1,9 +1,72 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Decode {
-    public static void textDecode(List<String> list, int key) {
+    public static List<Character> charsForDecoding;
+    public static List<Character> decodingChars;
+    public static List<String> decodingLines;
+    public static final List<Character> littleAlphabet = Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+    public static final List<Character> bigAlphabet = Arrays.asList('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+    public static List <String> textDecode(List<String> list, int key) {
+        charsForDecoding = toCharArray(list);
+        decodingChars = decoding(charsForDecoding, key);
+        decodingLines = decodingLines(decodingChars, list);
+        return  decodingLines;
+    }
+    public static List<Character> toCharArray(List<String> list) {
+        List<Character> charArray = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            String line = list.get(i);
+            for (int j = 0; j < line.length(); j++) {
+                charArray.add(line.charAt(j));
+            }
+        }
+        return charArray;
+    }
+    public static List<Character> decoding(List<Character> list, int key) {
+        int readyKey = key-1;
+        List<Character> encodingText = new ArrayList<>();
+        List<Character> withKeyLittleAlphabet = littleAlphabet;
+        List<Character> withKeyBigAlphabet = bigAlphabet;
+        for (int i = 0; i < list.size(); i++) {
+            char oneSymbol = list.get(i);
+            if(Character.isAlphabetic(oneSymbol)) {
+                for (int j = 0; j < littleAlphabet.size(); j++) {
+                    if (list.get(i) == littleAlphabet.get(j)) {
+                        Collections.rotate(withKeyLittleAlphabet, -readyKey);
+                        encodingText.add(withKeyLittleAlphabet.get(j));
+                        break;
+                    } else if (list.get(i) == bigAlphabet.get(j)) {
+                        Collections.rotate(withKeyBigAlphabet, -readyKey);
+                        encodingText.add(withKeyBigAlphabet.get(j));
+                        break;
+                    }
 
+                }
+            }
+            else {
+                encodingText.add(list.get(i));
+            }
+        }
+        return encodingText;
+    }
+    public static List<String> decodingLines(List<Character> decodingChars, List<String> firstText) {
+        List<String> decodingText = new ArrayList<>();
+        StringBuilder line = new StringBuilder(0);
+        int count = 0;
+        for (int i = 0; i < firstText.size(); i++) {
+            String textLine = firstText.get(i);
+            for (int j = 0; j < textLine.length(); j++) {
+                line.append(decodingChars.get(count));
+                count++;
+            }
+            decodingText.add(line.toString());
+            line.setLength(0);
+        }
+        return decodingText;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import java.io.FileReader;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,6 +10,7 @@ import java.util.List;
 public class Main {
     public static List<String> text = new ArrayList<>();
     public static List<String> encodingLines = new ArrayList<>();
+    public static List<String> decodingLines = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         int key = Integer.parseInt(args[2]);
         try {
@@ -25,7 +26,11 @@ public class Main {
             Files.write(path, encodingLines);
         }
         else if(args[0].equalsIgnoreCase("decode")){
-            Decode.textDecode(text, key);
+            decodingLines = Decode.textDecode(text, key);
+            String link = args[1];
+            String[] ArrayLink = link.split("\\(encoded\\)");
+            Path path = Files.createFile(Path.of(ArrayLink[0] + "(decoded)" + ArrayLink[1]));
+            Files.write(path, decodingLines);
         }
 
     }
