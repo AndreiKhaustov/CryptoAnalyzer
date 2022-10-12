87186ce6bc367d870b000b94344553a10ae176af
AndreiKhaustov <andrei.khaustov.nextlevel@gmail.com>
2022-10-12 16:18:58 +0300

    Added bruteForce method

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,6 +2,7 @@ package Main;
 
 
 import org.projectComponents.Decode;
+import org.projectComponents.DecodeByBruteForce;
 import org.projectComponents.Encode;
 
 import java.io.IOException;
@@ -12,16 +13,19 @@ import java.util.List;
 
 public class Main {
     public static List<String> text = new ArrayList<>();
+    public static List<String> textForComparing = new ArrayList<>();
     public static List<String> encodingLines = new ArrayList<>();
     public static List<String> decodingLines = new ArrayList<>();
+    public static List<String> decodingByBruteForce = new ArrayList<>();
     public static void main(String[] args) throws IOException {
-        int key = Integer.parseInt(args[2]);
+
         try {
             text = Files.readAllLines(Path.of(args[1]));
         } catch (IOException e) {
             System.out.println("Try to enter valid link!");
         }
         if(args[0].equalsIgnoreCase("encode")) {
+            int key = Integer.parseInt(args[2]);
            encodingLines = Encode.textEncode(text, key);
             String link = args[1];
             String[] ArrayLink = link.split("\\.");
@@ -30,6 +34,7 @@ public class Main {
             Files.write(path, encodingLines);
         }
         else if(args[0].equalsIgnoreCase("decode")){
+            int key = Integer.parseInt(args[2]);
             decodingLines = Decode.textDecode(text, key);
             String link = args[1];
             String[] ArrayLink = link.split("\\(encoded\\)");
@@ -38,7 +43,14 @@ public class Main {
             Files.write(path, decodingLines);
         }
         else if(args[0].equalsIgnoreCase("bruteForce")) {
-
+             textForComparing = Files.readAllLines(Path.of(args[2]));
+             decodingByBruteForce = DecodeByBruteForce.startBruteForce(text, textForComparing);
+             int key = DecodeByBruteForce.searchingKey(DecodeByBruteForce.getStatisticData(Decode.toCharArray(text)), DecodeByBruteForce.getStatisticData(Decode.toCharArray(textForComparing)));
+            String link = args[1];
+            String[] ArrayLink = link.split("\\(encoded\\)");
+            Files.deleteIfExists(Path.of(ArrayLink[0] + " decoded key-" + key + ArrayLink[1]));
+            Path path = Files.createFile(Path.of(ArrayLink[0] + " decoded key-" + key + ArrayLink[1]));
+            Files.write(path, decodingByBruteForce);
         }
 
     }
